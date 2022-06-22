add222fd62d95d00cbdbf4fd02ee1eda0b8ca223
Your Name <nafularespah@gmail.com>
2022-06-22 18:47:05 +0300

    changes

--- slice.py
+++ slice.py
@@ -1,4 +1,16 @@
-statement="childrenplaying"
-middlechar=len(statement)//2
-capitals=statement[middlechar:].upper() 
-print(capitals)
+str="childrenplaying"
+q=len(str)//2
+first=(str[:q]).upper()
+second=(str[q:])
+full_statement=first+second
+print(full_statement)
+
+# b="AkiraChix"
+# print(b[-5:-2])
+# print(b[0:])
+
+
+
+# statement="childrenplaying"
+# name=(len(statement))//2
+# first_name=
